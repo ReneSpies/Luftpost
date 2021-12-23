f3dc42dec654b670ebd6dc57af11270542abde31
René Jörg Spies <rene.j.spies@gmail.com>
2021-12-23 20:39:57 +0100

    LP-20
    Added Timber as dependency; created new Application `LuftpostApplication` and initialized Timber in it; added `LuftpostApplication` in the manifest

--- LuftpostAndroid/app/build.gradle.kts
+++ LuftpostAndroid/app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:${Versions.COMPOSE}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
     implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("com.jakewharton.timber:timber:5.0.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${Versions.COMPOSE}")
